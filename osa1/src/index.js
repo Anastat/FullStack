51fda28f43033743645a691dc0b5ec6156ef26ec
Anastat <anastat@metropolia.fi>
2018-09-09 19:08:36 +0300

    Tehtava 1.7

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -7,39 +7,56 @@ class App extends React.Component {
         this.state = {
             hyva: 0,
             neutraali: 0,
-            huono: 0
+            huono: 0,
+            summa: 0,
+            laskuri: 0,
+            positiivisia: 0
         }
+    }
+    laskuriKasvaa = () => {
+        this.setState({
+            laskuri: this.state.laskuri +1
+        })
     }
     clickHyva = () => {
         this.setState({
-            hyva: this.state.hyva + 1
-        })
+            hyva: this.state.hyva + 1,
+            summa: this.state.summa + 1,
+            positiivisia: this.state.positiivisia + 1
+        });
+        this.laskuriKasvaa()    
     }
     clickNeutraali = () => {
         this.setState({
             neutraali: this.state.neutraali + 1
-        })
+        });
+        this.laskuriKasvaa()
     }
     clickHuono = () => {
         this.setState({
-            huono: this.state.huono + 1
-        })
+            huono: this.state.huono + 1,
+            summa: this.state.summa - 1,
+        });
+        this.laskuriKasvaa() 
     }
 
     render () {
-        console.log(this.state.huono)
+        console.log(this.state.laskuri, this.state.summa)
         return (
             <div>
                 <div>
+                    <h2>Anna palautetta</h2>
                     <button onClick={this.clickHyva}>hyvä</button> 
                     <button onClick={this.clickNeutraali}>neutraali</button> 
                     <button onClick={this.clickHuono}>huono</button>
                 </div>
                 <div>
-                    <h2>statistiikka</h2>
+                    <h2>Statistiikka</h2>
                     <p>hyvä {this.state.hyva}</p>
                     <p>neutraali {this.state.neutraali}</p>
                     <p>huono {this.state.huono}</p>
+                    <p>keskiarvo {(this.state.summa/this.state.laskuri).toFixed(2)}</p>
+                    <p>positiivisia {(this.state.positiivisia*100/this.state.laskuri).toFixed(1)} %</p>
                 </div>
             </div>
             
