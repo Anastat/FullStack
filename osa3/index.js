7b2d270e117917f745ef5fbb4865a1bd8eb15997
Anastat <anastat@metropolia.fi>
2018-09-22 17:23:29 +0300

    Tehtava 3.6

--- osa3/index.js
+++ osa3/index.js
@@ -62,8 +62,12 @@ app.get('/info', (req, res) => {
 
   app.post('/api/persons', (req, res) => {
       const body = req.body
-      if (body.name == undefined) {
-          return res.status(400).json({error: 'name missing'})
+      if (body.name == undefined && body.numero == undefined) {
+          return res.status(400).json({error: 'name or number missing'})
+      }
+
+      if (persons.find(person => person.name === body.name)) {
+        return res.status(400).json({error: 'name must be unique'})
       }
 
       const person = {
