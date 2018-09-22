68c60cd6cbf58960b2503d12bc6f4f204148614d
Anastat <anastat@metropolia.fi>
2018-09-22 10:38:30 +0300

    Tehtava 3.3

--- osa3/index.js
+++ osa3/index.js
@@ -32,15 +32,25 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
   })
 
-  app.get('/info', (req, res) => {
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.get('/info', (req, res) => {
       const info = `puhelinluettelossa ${persons.length} henkilön tiedot`
-    //res.send('<p>info</p><p>new Date()</p>')
     
       res.send (info + '</br>' + new Date())
     
   })
 
-  const PORT = 3001
+const PORT = 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
